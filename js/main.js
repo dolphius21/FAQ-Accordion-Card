27d347db648964e2867ba620a11e212ecd8379d6
dolphius21 <johnrudolphlee@gmail.com>
2021-01-17 10:53:24 +0800

    refactor code for accordion functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
-const faqList = document.querySelector(".faq-list");
+const faqItems = document.querySelectorAll(".faq-item");
 
-faqList.addEventListener("click", showAnswer);
+faqItems.forEach((faqItem) => {
+  const text = faqItem.querySelector('.text');
+  
+  text.addEventListener('click', () => {
+    faqItems.forEach((item) => {
+      if (item !== text.parentElement) {
+        item.classList.remove('active');
+      }
+    });
 
-function showAnswer(e) {
-  if (e.target.nextElementSibling.classList.contains("a-active")) {
-    e.target.classList.remove("q-active");
-    e.target.nextElementSibling.classList.remove("a-active");
-    e.target.parentElement.nextElementSibling.classList.remove("down");
-  } else {
-    e.target.classList.add("q-active");
-    e.target.nextElementSibling.classList.add("a-active");
-    e.target.parentElement.nextElementSibling.classList.add("down");
-  }
-}
+    faqItem.classList.toggle('active');
+  });
+});
+
+
